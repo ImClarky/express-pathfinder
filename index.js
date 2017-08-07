bc1b01a13f86fae8cb704cef4b797eb32a24533d
Sean Clark <imclarky@users.noreply.github.com>
2017-08-07 14:00:47 +0100

    update to ES6 and remove unnecessary lines

--- index.js
+++ index.js
@@ -1,22 +1,20 @@
-var _methods = require('http').METHODS
+const _methods = require('http').METHODS
 
-module.exports = function(routes, methods) {
+module.exports = (routes, methods) => {
 
   if (typeof methods == "undefined") {
     methods = _methods
   } else if (typeof methods == "string") {
-    methods = methods.toUpperCase()
-    if (_methods.indexOf(methods) === -1) {
-      throw new Error("The method you have provided: '" + methods + "' is not supported. Please use one of the following methods:\r\n\r\n" + _methods.join(", "))
+    if (_methods.indexOf(methods.toUpperCase()) === -1) {
+      throw new Error("The method you have provided: '" + methods + "' is not supported. Please use one of the following methods:\r\n" + _methods.join(", "))
       return
     }
   } else if (typeof methods == "object" && Array.isArray(methods)) {
-    var errors = [];
-    var initialCount = methods.length
-    methods = methods.map(function(method) {
-      method = method.toUpperCase()
-      if (_methods.indexOf(method) === -1) {
-        errors.push(method)
+    let errors = []
+    let initialCount = methods.length
+    methods = methods.map(method => {
+      if (_methods.indexOf(method.toUpperCase()) === -1) {
+        errors.push(method.toUpperCase())
         return
       }
       return method
@@ -34,12 +32,12 @@ module.exports = function(routes, methods) {
     return
   }
 
-  var list = [],
-    route
+  let list = [],
+      route
 
-  routes.forEach(function(middleware) {
+  routes.forEach(middleware => {
     if (middleware.route) {
-      var associatedMethod = Object.keys(middleware.route.methods)[0].toUpperCase() //object should only have one key - the method
+      let associatedMethod = Object.keys(middleware.route.methods)[0].toUpperCase() //object should only have one key - the method
       if (methods.indexOf(associatedMethod) !== -1) {
         route = {}
         route.method = associatedMethod
@@ -48,18 +46,13 @@ module.exports = function(routes, methods) {
         list.push(route)
       }
     } else if (middleware.name === "router") {
-
-      /** gets the first part of the uri that app._router.stack forgets
-       * for example:
-       *     app.use('/users', users)
-       * would ommit the '/users' part in the uris - not giving you the full path
-       */
-      var mount = middleware.regexp.toString().match(/([\/\\A-Za-z0-9-_]+)\?/)[1]
+      // gets the first part of the uri that app._router.stack forgets
+      let mount = middleware.regexp.toString().match(/([\/\\\w-]+)\?/)[1]
       mount = mount.replace(/\\/g, "")
       mount = mount.replace(/\/$/, "")
 
-      middleware.handle.stack.forEach(function(handler) {
-        var associatedMethod = Object.keys(handler.route.methods)[0].toUpperCase() //object should only have one key - the method
+      middleware.handle.stack.forEach(handler => {
+        let associatedMethod = Object.keys(handler.route.methods)[0].toUpperCase() //object should only have one key - the method
         if (methods.indexOf(associatedMethod) !== -1) {
           route = {}
           route.method = associatedMethod
